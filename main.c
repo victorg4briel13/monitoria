19b70215e5479d9d18171500b506b206a9bb896e
victorg4briel13 <82788133+victorg4briel13@users.noreply.github.com>
2023-04-18 11:33:06 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 int main()
 {
+    system("color a");
+    //Menu inicial
     printf("BEM VINDO \nEscolha uma opsao\
+           \n1-Login \
+           \n2-Cadastro \
+           \n3-Sair\n");
+    system("pause");
+    system("cls");
+
+    //Menu principal
+    printf("\n\nO que deseja fazer? \
            \n1-Perfil \
            \n2-Tarefas \
            \n3-Materias\
            \n4-Planos de estudos\
            \n5-Visao geral\
-           \n6-Encerrar");
+           \n6-configursao\
+           \n7-Encerrar");
 }
