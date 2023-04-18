5a3c298d86ad07a106f1b00e422f87363674e544
victorg4briel13 <82788133+victorg4briel13@users.noreply.github.com>
2023-04-18 11:14:53 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("BEM VINDO \nEscolha uma opsao\
+           \n1-Perfil \
+           \n2-Tarefas \
+           \n3-Materias\
+           \n4-Planos de estudos\
+           \n5-Visao geral\
+           \n6-Encerrar");
+}
